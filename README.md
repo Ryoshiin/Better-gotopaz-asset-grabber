931eb560aabfdd41b49b5f5917eaa665ab190f53
notmyname <bcboos05@gmail.com>
2022-05-27 18:05:35 +0200

    Minor changes, added some logging, and a lot of commenting because that's funny

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # A simple tool to download the assets from Gotopazu
- 
- A very simple tool to easily get assets which can then be viewed with [AssetStudio](https://github.com/Perfare/AssetStudio), just open the files in there and view them, it's that easy. Just grab the newest release from the [releases](https://github.com/notmynamex/gotopaz-asset-grabber/releases/latest) page!
- 
+
+ A very simple tool to easily get assets which can then be viewed with [AssetStudio](https://github.com/Perfare/AssetStudio), get their newest release from their [releases page](https://github.com/Perfare/AssetStudio/releases/latest) and just open the files in there and view them, it's that easy. Just grab the newest release from the [releases](https://github.com/notmynamex/gotopaz-asset-grabber/releases/latest) page!
+
  Only requirements are:
- - Wget on path (download newest version on [the windows mirror page](http://gnuwin32.sourceforge.net/packages/wget.htm). Also here a [short tutorial](https://www.addictivetips.com/windows-tips/install-and-use-wget-in-windows-10/) on how to add it to path.
+
+- Wget on path (download this specific version from [here](https://sourceforge.net/projects/getgnuwin32/files/wget-1.12.1-devel-and-openssl-1.0.0a.zip) and put it in C:\Program Files\GnuWin32. Also here a [short tutorial](https://www.addictivetips.com/windows-tips/install-and-use-wget-in-windows-10/) on how to add it to path, just select the "GnuWin32" folder itself instead of the bin folder, since the bin folder doesn't exist.
 - Eyes and the ability to remember/copy a version number on the Google Play Store.
 
-and nothing else really.
-I wrote this in like 20 minutes, if there are any issues open an issue.
+and nothing else really, have fun!
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# this is not overcommented at all
 import re,requests,os,time,random,logging
 
 logging.basicConfig(
@@ -5,35 +6,47 @@ logging.basicConfig(
     format='%(levelname)s: %(message)s')
 
 outputDir = "unity2d_all"
+# i should make the output dir name configurable, but i'm lazy
 version = str(input('Version (Find it at https://play.google.com/store/apps/details?id=jp.enish.gotopazu, then open the "About this game" option, https://files.catbox.moe/et7cek.png and https://files.catbox.moe/k3iwsp.png for reference): '))
+# i should make this a gui, but i'm lazy and i don't know how
+# i should get the version from the website, but i'm lazy
+# i should make it remember the last entered version, but i'm lazy
 version = version.replace('.', '_')
-print(version)
+logging.info(f"Version: {version}")
 if not version:
-    print('No version specified, exiting...')
+    logging.info('No version specified, exiting...')
     exit()
+    # no version specified, exit
 url = f"https://www-cancer.enish-games.com/v{version}/resource/list/Android"
-print(url)
+logging.info(f"Download url: {url}")
 response = requests.get(url).text
-print(response)
 if "メンテナンス中" in response:
     raise Exception("Server is in maintenance")
+    # server is in maintenance, exit
 if not os.path.exists(outputDir):
-    os.mkdir("unity2d_all")
+    os.mkdir(outputDir)
+    # create output directory if it doesn't exist
 fileNames = set(re.findall(r"[0-9a-f]{32}", response))
+# find all file names in the response
 finddir = set(os.listdir("unity2d_all"))
+# find all file names in the output directory
 filelist = list(fileNames-finddir)
-print(filelist)
+# find the difference between the two sets
 for file in filelist:
     if file in outputDir:
         logging.info(f"{file} already exists")
         continue
+        # if file already exists, skip
     else:
-        url = "https://assets.enish-games.com/assets-cancer/Resources/android/"+file
+        url = f"https://assets.enish-games.com/assets-cancer/Resources/android/{file}"
+        logging.info(f"Downloading {file}")
         os.system(
             " ".join([
                 'wget',
                 '--random-wait',
                 '--no-check-certificate',
+                '-q',
+                '-nv',
                 '-c',
                 '-N',
                 f'--user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53"',
@@ -43,3 +56,11 @@ for file in filelist:
             ])
         )
         time.sleep(random.uniform(0.1, 1))
+        logging.info(f"{file} downloaded")
+        # download file and log it
+if not file in filelist:
+    logging.info("All files are downloaded")
+    exit()
+    # all files are downloaded, exit
+# cant believe all this actually works
+# hey, if you're reading this, i hope you know what you're doing, because i don't, so don't complain
